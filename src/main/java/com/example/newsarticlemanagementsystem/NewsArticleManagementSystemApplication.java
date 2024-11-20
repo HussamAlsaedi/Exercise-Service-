429eadf19db6b14f341d84c5c437d26d2fa07232
Hussam Alsaedi <126284518+HussamAlsaedi@users.noreply.github.com>
2024-11-20 20:35:35 +0300

    Update NewsArticleManagementSystemApplication.java

--- src/main/java/com/example/newsarticlemanagementsystem/NewsArticleManagementSystemApplication.java
+++ src/main/java/com/example/newsarticlemanagementsystem/NewsArticleManagementSystemApplication.java
@@ -1,13 +1,87 @@
-package com.example.newsarticlemanagementsystem;
+package com.example.newsarticlemanagementsystem.NewsArticleService;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.newsarticlemanagementsystem.Model.NewsArticle;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@SpringBootApplication
-public class NewsArticleManagementSystemApplication {
+import java.util.ArrayList;
+import java.util.Date;
 
-    public static void main(String[] args) {
-        SpringApplication.run(NewsArticleManagementSystemApplication.class, args);
+
+@Service
+public class NewsArticleService {
+
+    private final ArrayList<NewsArticle> articles = new ArrayList<>();
+
+
+    public ArrayList<NewsArticle> getAllArticles() {
+        return articles;
+    }
+
+    public boolean addArticle(NewsArticle newsArticle)
+    {
+        if(articles.add(newsArticle))
+        {
+            return true;
+        }
+        return false;
+    }
+
+   public boolean updateArticle(String id, NewsArticle updatedArticle) {
+        for (int i = 0; i < articles.size(); i++) {
+            if (articles.get(i).getId().equals(id)) {
+                articles.set(i, updatedArticle);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteArticle(String id)
+    {
+        for (int i = 0; i < articles.size(); i++) {
+            if (articles.get(i).getId().equals(id)) {
+                articles.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean publishArticle(String id) {
+        for (NewsArticle newsArticle : articles) {
+            if (newsArticle.getId().equals(id)) {
+                newsArticle.setPublished(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public ArrayList<NewsArticle> getPublishedArticles()
+    {
+        ArrayList<NewsArticle> publishedArticles = new ArrayList<>();
+
+        for (NewsArticle newsArticle : articles) {
+            if (newsArticle.isPublished()) {
+                publishedArticles.add(newsArticle);
+            }
+        }
+        return publishedArticles;
+    }
+
+
+    public ArrayList GetNewsArticlesByCategory(String  Category) {
+
+        ArrayList matchingCategory = new ArrayList();
+
+        for (NewsArticle newsArticle: articles) {
+            if (newsArticle.getCategory().equalsIgnoreCase(Category)) {
+                matchingCategory.add(newsArticle);
+            }
+        }
+        return matchingCategory;
     }
 
 }
